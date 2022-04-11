7795c5e4f8529be7fd43da88ede5f7cdfd8974cf
Darya9810 <93486920+Darya9810@users.noreply.github.com>
2022-04-11 23:56:12 +0300

    CI

--- src/test/java/ru/netology/rest/MobileBankApiTestV2.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -8,14 +8,18 @@ import static io.restassured.RestAssured.given;
 class MobileBankApiTestV2 {
     @Test
     void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
         given()
                 .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
                 .when()
                 .get("/demo/accounts")
+                // Проверки
                 .then()
                 .statusCode(200)
                 .header("Content-Type", "application/json; charset=UTF-8")
-                .contentType(ContentType.JSON)
-        ;
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON);
     }
 }
